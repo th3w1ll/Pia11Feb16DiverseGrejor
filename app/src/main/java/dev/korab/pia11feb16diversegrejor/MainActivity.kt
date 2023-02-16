1bbfe6fcbc5d01bd32ce972019dc8dae67039e4d
th3w1ll <patrik.korab@gmail.com>
2023-02-16 09:17:18 +0100

    Start

---
+++ app/src/main/java/dev/korab/pia11feb16diversegrejor/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.korab.pia11feb16diversegrejor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
